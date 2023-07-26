835dd3f82aff8a01d77025995fc696ad721af701
JuneMuoti <june.muoti@gmail.com>
2023-07-26 16:16:12 +0300

    Edited Dates for campaign

--- frontend/views/site/terms.php
+++ frontend/views/site/terms.php
@@ -25,35 +25,40 @@ $this->params['breadcrumbs'][] = $this->title;
                     <div style="margin-left: 30px;">
                     <p>1.1. The Skiza service is a personalized ring back tone service whereby you can download or record a ringtone. </p>
                     <p>1.2. The Skiza promotion is available to both Post & Pre pay subscribers available on Safaricom network.</p>
-                    <p>1.3. The promotion will run through Mtech’s Facebook social media account. The account is titled <b>  Mtech Music ke</b>.</p>
-                    <p>1.4. Customers will dial the provided Safaricom Skiza shortcut code <b>*811*998#</b> to download the tune. The downloaded ringtone will be played to & heard by the customers called (both safaricom subscribers & callers on other networks) from the time the connection to your line is made until when you pick up or disconnect the call.</p>
-                    <p>1.5. The cost per tune will be as per the set Skiza price by Safaricom at <b>KES 1.50 per day.</b> The same is on daily auto renewal for a period of 30 days.</p>
-                    <p>1.6. Upon downloading the tune the customers will receive a notification message from <b>‘SKIZA’</b>, showing the song downloaded, cost of tune renewal and how to unsubscribe from the tune.</p>
-                    <p>1.7. Customers will be required to forward the received message from skiza to our short code number <b>‘20683’</b>, which will be used for the various draws to win the cash prize.</p>
-                    <p>1.8. The promotion will run for a period of 3 months from <b>2nd July – 2nd October 2023.</b></p>
-                    <p>1.9. There shall be monthly draws held to determine the monthly winners at the end of every month.
+                    <p>1.3. All participants must be Kenyan citizen’s 18 years &amp; above, must hold a valid ID card &amp; a valid KRA Pin.</p>
+                    <p>1.4. The promotion will run through Mtech’s Facebook social media account. The account is titled <b>  Mtech Music ke</b>.</p>
+                    <p>1.5. Customers will dial the provided Safaricom Skiza shortcut code <b>*811*998#</b> to download the tune. The downloaded ringtone will be played to & heard by the customers called (both safaricom subscribers & callers on other networks) from the time the connection to your line is made until when you pick up or disconnect the call.</p>
+                    <p>1.6. The cost per tune will be as per the set Skiza price by Safaricom at <b>KES 1.50 per day.</b> The same is on daily auto renewal for a period of 30 days.</p>
+                    <p>1.7. Upon downloading the tune the customers will receive a notification message from <b>‘SKIZA’</b>, showing the song downloaded, cost of tune renewal and how to unsubscribe from the tune.</p>
+                    <p>1.8. Customers will be required to forward the received message from skiza to our short code number <b>‘20683’</b>, which will be used for the various draws to win the cash prize.</p>
+                    <p>1.9. The promotion will run for a period of 3 months from <b>27th July – 19th October 2023.</b></p>
+                    <p>1.10. There shall be monthly draws held to determine the monthly winners at the end of every month.
                         The monthly draws will build up to the final draw for the grand cash prize.
                         The draws will be held via our online portal where the mobile numbers of all the participants will be included in the pool for selection of winners.</p>
-                    <p>1.10. The draws will be held as below;</p>
+                    <p>1.11. The draws will be held as below;</p>
 
                     <ul style="font-weight: bold;">
-                        <li>1st draw date – 4th August </li>
-                        <li>2nd draw date – 4th September </li>
-                        <li>3rd draw date – 6th October</li>
+                        <li>1st draw date – 28th August </li>
+                        <li>2nd draw date – 25th September </li>
+                        <li>3rd draw date – 18th October</li>
+                        <li>Grand Draw – 19th October</li>
                     </ul>
 
-                    <p>1.11.  Prizes to be won shall be as below;</p>
+                    <p>1.12.  Prizes to be won shall be as below;</p>
 
                         <ul style="font-weight: bold;">
-                            <li>1st prize-KES 20,000 </li>
+                            <li>1st Prize-KES 20,000 </li>
                             <li>2nd Prize – KES 20,000 </li>
-                            <li>3rd Prize – KES 20,000 & KES 40,000 </li>
+                            <li>3rd Prize – KES 20,000 </li>
+                            <li>Grand Prize -KES 40,000</li>
                         </ul>
 
-                    <p>1.12. Once the winners are selected the cash prizes shall be disbursed via Mtech’s registered Paybill number, which is registered under the name <b>MTECH LIMITED</b>. Winners will receive their Net cash prizes via their M-pesa registered numbers after all applicable tax deductions have been made. </p>
-                    <p>1.13. Winners should claim their cash prize within 14 working days.</p>
-                    <p>1.14. In case of any disputes arising from these promotions, participants can send an email to <a href="mailto:contentkenya@mtechcomm.co.ke">contentkenya@mtechcomm.co.ke</a> or call <b>  0712222299</b>.</p>
-                    <p>1.15. Safaricom reserves the right to vary or amend any features of the Skiza Service.Such changes shall be advertised on Safaricom’s network and may also be published in the local press. Such changes shall not affect ring-back tones that participants have purchased as at the date of the changes.</p>
+                    <p>1.13. Once the winners are selected the cash prizes shall be disbursed via Mtech’s registered Paybill number, which is registered under the name <b>MTECH LIMITED</b>. Winners will receive their Net cash prizes via their M-pesa registered numbers after all applicable tax deductions have been made. </p>
+                    <p>1.14. Winners should claim their cash prize within 14 working days.</p>
+                    <p>1.15. In case of any disputes arising from these promotions, participants can send an email to <a href="mailto:contentkenya@mtechcomm.co.ke">contentkenya@mtechcomm.co.ke</a> or call <b>  0712222299</b>.</p>
+                    <p>1.16. Safaricom reserves the right to vary or amend any features of the Skiza Service.Such changes shall be advertised on Safaricom’s network and may also be published in the local press. Such changes shall not affect ring-back tones that participants have purchased as at the date of the changes.</p>
+                    <p>1.17. All Mtech employees and their families are not allowed to participate in this skiza promotion campaign. </p>
+
                     </div>
                     <h3>2. PERSONAL DATA PROTECTION </h3>
                     <p>2.1.    So far Mtech processes any personal data (including name, mobile/telephone details, and other contact or personal details) relating to customers participating in the promotion. which is acquired or collected during the promotion, Mtech shall:</p>
